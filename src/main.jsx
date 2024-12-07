eb224ba0089253837eb3defbd2f64b96d90c78f7
arsalanlal414 <arsalanbaba414@yahoo.com>
2024-12-07 15:40:49 +0500

    removed unnecessary code

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { DarkModeProvider } from './hooks/DarkModeContext.jsx'
-import { ApiProvider } from './hooks/ApiContext.jsx'
 import { DataProvider } from './hooks/DataContext.jsx';
 
 createRoot(document.getElementById('root')).render(
